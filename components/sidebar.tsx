eeb59ca9185b3f12ebcfe58c0b9144e7b64840fd
Nikunj Mehra <nikunjmehra935@gmail.com>
2023-11-11 19:10:39 +0530

    end of day 1

---
+++ components/sidebar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { LayoutDashboard } from "lucide-react";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] })
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500"
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image
+              fill
+              alt="Logo"
+              src="/logo.png"
+            />
+          </div>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>Genius</h1>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
